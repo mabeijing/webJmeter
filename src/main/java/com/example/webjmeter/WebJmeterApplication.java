24a4a6fcb9b9ee0111ec30626de2216c7d6e371e
beijingm <58149278@qq.com>
2024-03-05 16:02:12 +0800

    首次提交

---
+++ src/main/java/com/example/webjmeter/WebJmeterApplication.java
@@ -0,0 +1,13 @@
+package com.example.webjmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJmeterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebJmeterApplication.class, args);
+    }
+
+}
